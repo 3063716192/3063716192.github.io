fbef8b7f01423a00dc0c3495600a740a918e4006
3063716192 <3063716192@qq.com>
2025-04-20 18:55:43 +0800

    Site updated: 2025-04-20 18:55:39

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/04/20/欢迎大家来到我的个人博客/"];function toRandomPost(){
+var posts=["2025/04/20/今天天气真好！☀️/","2025/04/20/欢迎大家来到我的个人博客/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
